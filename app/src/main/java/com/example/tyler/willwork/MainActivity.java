8cbf915a0aa6ca148ec193963c5cb077b1daf62c
tjander3 <tjander22@gmail.com>
2016-04-21 15:33:48 -0400

    first commit

--- app/src/main/java/com/example/tyler/willwork/MainActivity.java
+++ app/src/main/java/com/example/tyler/willwork/MainActivity.java
@@ -24,40 +24,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         listen2(linearLayout);
     }
 
-    public void onClick(View v) {
-
-        switch (v.getId()) {
-            case 0:
-                ((TextView)findViewById(R.id.textView)).setText("0");
-                break;
-            case 1:
-                ((TextView)findViewById(R.id.textView)).setText("1");
-                break;
-            case 2:
-                ((TextView)findViewById(R.id.textView)).setText("2");
-                break;
-            case 3:
-                ((TextView)findViewById(R.id.textView)).setText("3");
-                break;
-            case 4:
-                ((TextView)findViewById(R.id.textView)).setText("4");
-                break;
-            case 5:
-                ((TextView)findViewById(R.id.textView)).setText("5");
-                break;
-            case 6:
-                ((TextView)findViewById(R.id.textView)).setText("6");
-                break;
-            case 7:
-                ((TextView)findViewById(R.id.textView)).setText("7");
-                break;
-            case 8:
-                ((TextView)findViewById(R.id.textView)).setText("8");
-                break;
-            case 9:
-                ((TextView)findViewById(R.id.textView)).setText("9");
-                break;
-        }
+    private void addToHorzScrollView2(LinearLayout linearLayout, int count) {
+        //LinearLayout linearLayout = (LinearLayout) findViewById(R.id.mylin);
+        ImageButton myImage = new ImageButton(this);
+        myImage.setBackground(null);  //Get rid of grey border
+        myImage.setId(count);
+        myImage.setImageResource(R.drawable.ppp);
+        myImage.setLayoutParams(new AbsListView.LayoutParams(
+                AbsListView.LayoutParams.MATCH_PARENT,
+                AbsListView.LayoutParams.WRAP_CONTENT));
+        linearLayout.addView(myImage);
     }
 
     private void listen2(LinearLayout linearLayout){
@@ -83,98 +59,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         iB9.setOnClickListener(this);
     }
 
-    private void listen(LinearLayout linearLayout){
-        ImageButton iB0 = (ImageButton)linearLayout.findViewById(0);
-        iB0.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                ((TextView)findViewById(R.id.textView)).setText("zero");
-                // Perform action on click
-            }
-        });
-
-        ImageButton iB1 = (ImageButton)linearLayout.findViewById(1);
-        iB1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                ((TextView)findViewById(R.id.textView)).setText("1");
-                // Perform action on click
-            }
-        });
-        ImageButton iB2 = (ImageButton)linearLayout.findViewById(2);
-        iB2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                ((TextView)findViewById(R.id.textView)).setText("2");
-                // Perform action on click
-            }
-        });
-        ImageButton iB3 = (ImageButton)linearLayout.findViewById(3);
-        iB3.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                ((TextView)findViewById(R.id.textView)).setText("3");
-                // Perform action on click
-            }
-        });
-        ImageButton iB4 = (ImageButton)linearLayout.findViewById(4);
-        iB4.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                ((TextView)findViewById(R.id.textView)).setText("4");
-                // Perform action on click
-            }
-        });
-        ImageButton iB5 = (ImageButton)linearLayout.findViewById(5);
-        iB5.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                ((TextView)findViewById(R.id.textView)).setText("5");
-                // Perform action on click
-            }
-        });
-        ImageButton iB6 = (ImageButton)linearLayout.findViewById(6);
-        iB6.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                ((TextView)findViewById(R.id.textView)).setText("6");
-                // Perform action on click
-            }
-        });
-        ImageButton iB7 = (ImageButton)linearLayout.findViewById(7);
-        iB7.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                ((TextView)findViewById(R.id.textView)).setText("7");
-                // Perform action on click
-            }
-        });
-        ImageButton iB8 = (ImageButton)linearLayout.findViewById(8);
-        iB8.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                ((TextView)findViewById(R.id.textView)).setText("8");
-                // Perform action on click
-            }
-        });
-        ImageButton iB9 = (ImageButton)linearLayout.findViewById(9);
-        iB9.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                ((TextView)findViewById(R.id.textView)).setText("9");
-                // Perform action on click
-            }
-        });
-    }
-    public void addToHorzScrollView(LinearLayout linearLayout) {
-        //LinearLayout linearLayout = (LinearLayout) findViewById(R.id.mylin);
-        ImageView myImage = new ImageView(this);
-        myImage.setImageResource(R.drawable.ppp);
-        myImage.setLayoutParams(new AbsListView.LayoutParams(
-                AbsListView.LayoutParams.MATCH_PARENT,
-                AbsListView.LayoutParams.WRAP_CONTENT));
-        linearLayout.addView(myImage);
-    }
-
-    public void addToHorzScrollView2(LinearLayout linearLayout, int count) {
-        //LinearLayout linearLayout = (LinearLayout) findViewById(R.id.mylin);
-        ImageButton myImage = new ImageButton(this);
-        myImage.setBackground(null);  //Get rid of grey border
-        myImage.setId(count);
-        myImage.setImageResource(R.drawable.ppp);
-        myImage.setLayoutParams(new AbsListView.LayoutParams(
-                AbsListView.LayoutParams.MATCH_PARENT,
-                AbsListView.LayoutParams.WRAP_CONTENT));
-        linearLayout.addView(myImage);
+    public void onClick(View v) {
+        TextView tv = (TextView)findViewById(R.id.textView3);
+        switch (v.getId()) {
+            case 0:
+                tv.setText("0");
+                break;
+            case 1:
+                tv.setText("1");
+                break;
+            case 2:
+                tv.setText("2");
+                break;
+            case 3:
+                tv.setText("3");
+                break;
+            case 4:
+                tv.setText("4");
+                break;
+            case 5:
+                tv.setText("5");
+                break;
+            case 6:
+                tv.setText("6");
+                break;
+            case 7:
+                tv.setText("7");
+                break;
+            case 8:
+                tv.setText("8");
+                break;
+            case 9:
+                tv.setText("9");
+                break;
+        }
     }
 }
